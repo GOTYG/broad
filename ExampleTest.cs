a550a5857d2165193b78a71cfb3e3d2bc1a265e0
Shane <shane.everitt.murphy@gmail.com>
2025-01-15 18:42:53 -0800

    Add build job

--- ExampleTest.cs
+++ ExampleTest.cs
@@ -1,13 +1,16 @@
+using System.Diagnostics.CodeAnalysis;
 using GdUnit4;
 
 namespace Broad;
 
 [TestSuite]
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public class ExampleTest
 {
-   [TestCase]
-   public static void TestFoo()
-   {
-	  Assertions.AssertThat(5).IsEqual(5);
-   } 
+    [TestCase]
+    public static void TestFoo()
+    {
+        Assertions.AssertThat(5).IsEqual(5);
+    }
 }
